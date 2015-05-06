9660e4493da945b03e3d1f28322a2684008a70b4
Colin Jackson <colin.s.jackson90@gmail.com>
2015-05-06 09:48:47 -0400

    Do not auto-detect "binary" apps
    Buildpack must be specified manually.
    
    Signed-off-by: Matt Horan <mhoran@pivotal.io>

--- cf_spec/integration/deploy_a_binary_app_spec.rb
+++ cf_spec/integration/deploy_a_binary_app_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'CF Binary Buildpack' do
-  subject(:app) { Machete.deploy_app(app_name) }
   let(:browser) { Machete::Browser.new(app) }
 
   after do
@@ -9,13 +8,27 @@ describe 'CF Binary Buildpack' do
   end
 
   describe 'deploying a Ruby script' do
-    subject(:app) { Machete.deploy_app(app_name, stack: 'lucid64') }
     let(:app_name) { 'webrick_app' }
 
-    specify do
-      browser.visit_path('/')
+    context 'when specifying a buildpack' do
+      let(:app) { Machete.deploy_app(app_name, buildpack: 'binary-test-buildpack') }
 
-      expect(browser.body).to include('Hello, world!')
+      it 'deploys successfully' do
+        expect(app).to be_running
+
+        browser.visit_path('/')
+
+        expect(browser.body).to include('Hello, world!')
+      end
+    end
+
+    context 'without specifying a buildpack' do
+      let(:app) { Machete.deploy_app(app_name) }
+
+      it 'fails to stage' do
+        expect(app).not_to be_running
+        expect(app).to have_logged("An app was not successfully detected by any available buildpack")
+      end
     end
   end
 end
