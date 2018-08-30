825d3a6f39ee3bcce1e60bcddbf521bac2a65a4c
Ben Fickes <bfickes@pivotal.io>
2018-08-30 15:15:06 -0400

    Do not use readlink -f
    - Does not work on macs
    
    Co-authored-by: Tyler Phelan <tphelan@pivotal.io>

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export ROOT=`dirname $(readlink -f ${BASH_SOURCE%/*})`
+export ROOT="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
 $ROOT/scripts/install_tools.sh
 
 cd $ROOT/src/binary/
