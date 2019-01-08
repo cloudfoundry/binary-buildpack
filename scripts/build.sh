712db456eee5030221acdd2644c8a7744030c192
Tyler Phelan <tphelan@pivotal.io>
2019-01-08 09:39:48 -0500

    Vendor with go modules
    [#162654926]
    
    Co-authored-by: Don Nguyen <dnguyen@pivotal.io>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 source .envrc
 
-GOOS=linux go build -ldflags="-s -w" -o bin/supply ./src/binary/supply/cli
-GOOS=linux go build -ldflags="-s -w" -o bin/finalize ./src/binary/finalize/cli
+GOOS=linux go build -mod=vendor -ldflags="-s -w" -o bin/supply ./src/binary/supply/cli
+GOOS=linux go build -mod=vendor -ldflags="-s -w" -o bin/finalize ./src/binary/finalize/cli
