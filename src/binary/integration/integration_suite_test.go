a41aa7cca3af14bdbb4570444b64e4f2236e002e
Ryan Moran <rmoran@vmware.com>
2022-09-28 15:33:24 -0700

    Adds --stack flag for integration suite

--- src/binary/integration/integration_suite_test.go
+++ src/binary/integration/integration_suite_test.go
@@ -19,6 +19,7 @@ var buildpackVersion string
 var packagedBuildpack cutlass.VersionedBuildpackPackage
 var token string
 var platform string
+var stack string
 
 var _ = func() bool {
 	testing.Init()
@@ -32,6 +33,7 @@ func init() {
 	flag.StringVar(&cutlass.DefaultDisk, "disk", "64M", "default disk for pushed apps")
 	flag.StringVar(&token, "github-token", "", "use the token to make GitHub API requests")
 	flag.StringVar(&platform, "platform", "cf", `switchblade platform to test against ("cf" or "docker")`)
+	flag.StringVar(&stack, "stack", "cflinuxfs3", "stack to use when pushing apps")
 	flag.Parse()
 }
 
